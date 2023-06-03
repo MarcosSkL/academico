39c59c7828c62d8718494569362bbf16d0f46f28
Marcosskl <marcos.marsk@gmail.com>
2023-06-03 19:38:16 -0300

    add rota Alunos

---
+++ src/pages/alunos/[id].js
@@ -0,0 +1,100 @@
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Col, Row, Table, Form } from 'react-bootstrap'
+import Pagina from '../../components/Pagina'
+import { useForm } from "react-hook-form";
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+const FormAlterAlunos = () => {
+
+    const { push, query } = useRouter()
+    const { register, handleSubmit, setValue } = useForm()
+
+    useEffect(() => {
+
+        if (query.id) {
+
+            axios.get('/api/alunos/' + query.id).then(resultado => {
+                const aluno = resultado.data
+
+                for (let atributo in aluno) {
+                    setValue(atributo, aluno[atributo])
+                }
+            })
+        }
+
+    }, [query.id])
+
+    function salvar(dados) {
+        axios.put('/api/alunos/' + dados.id, dados)
+        push('/alunos')
+    }
+
+    return (
+
+        <Pagina titulo="Formulário">
+            <Row className="px-1 mx-1">
+                <Col>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="Nome">
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control type="text" placeholder="Nome" {...register('nome')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Cpf">
+                            <Form.Label>CPF</Form.Label>
+                            <Form.Control type="number" placeholder="CPF" {...register('cpf')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Matricula">
+                            <Form.Label>Matricula</Form.Label>
+                            <Form.Control type="number" placeholder="Matricula" {...register('matricula')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Email">
+                            <Form.Label>E-mail</Form.Label>
+                            <Form.Control type="text" placeholder="E-mail" {...register('email')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Telefone">
+                            <Form.Label>Telefone</Form.Label>
+                            <Form.Control type="text" placeholder="Telefone" {...register('telefone')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Cep">
+                            <Form.Label>CEP</Form.Label>
+                            <Form.Control type="number" placeholder="CEP" {...register('cep')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Logradouro">
+                            <Form.Label>Logradouro</Form.Label>
+                            <Form.Control type="text" placeholder="Logradouro" {...register('logradouro')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Complemento">
+                            <Form.Label>Complemento</Form.Label>
+                            <Form.Control type="text" placeholder="Complemento" {...register('complemento')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Numero">
+                            <Form.Label>Numero</Form.Label>
+                            <Form.Control type="number" placeholder="Numero" {...register('numero')} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="Bairro">
+                            <Form.Label>Bairro</Form.Label>
+                            <Form.Control type="text" placeholder="Bairro" {...register('bairro')} />
+                        </Form.Group>
+
+
+
+                        <div className='flex gap-3 justify-center'>
+                            <Button variant="primary" onClick={handleSubmit(salvar)}>
+                                Salvar
+                            </Button>
+
+                            <Link href={'/alunos'} className='btn btn-primary gap-2 text-white'>Voltar</Link>
+                        </div>
+
+                    </Form>
+
+                </Col>
+
+            </Row>
+        </Pagina>
+    )
+}
+
+export default FormAlterAlunos
---
+++ src/pages/alunos/index.js
@@ -0,0 +1,91 @@
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Col, Row, Table } from 'react-bootstrap'
+import Pagina from '../../components/Pagina'
+import { AiOutlineDelete, AiFillEdit } from 'react-icons/ai'
+import axios from 'axios'
+
+const index = () => {
+
+    const [alunos, setAlunos] = useState([])
+
+    useEffect(() => {
+        getAll()
+    }, [])
+
+    function getAll() {
+
+        axios.get('/api/alunos').then(resultado => {
+            setAlunos(resultado.data)
+
+        })
+    }
+
+
+    function excluir(id) {
+        if (confirm("Deseja excluir o registro?")) {
+            axios.delete('/api/alunos/' + id)
+            getAll()
+        }
+    }
+
+    return (
+
+        <Pagina titulo="Alunos">
+            <Row className="px-1 mx-1">
+                <Col>
+                    <Link href={'alunos/form/'} className='btn btn-primary text-white'>Novo</Link>
+                    <Table bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nome</th>
+                                <th>CPF</th>
+                                <th>Matricula</th>                        
+                                <th>Email</th>
+                                <th>Telefone</th>
+                                <th>CEP</th>
+                                <th>Logradouro</th>
+                                <th>Complemento</th>
+                                <th>Numero</th>
+                                <th>Bairro</th>
+
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {alunos.map((item, id) => (
+                                <tr key={item.id}>
+                                    <td className='flex gap-3'>
+                                        <Link href={'/alunos/' + item.id}>
+                                            <AiFillEdit className='ms-2 text-primary' />
+                                        </Link>
+                                        <AiOutlineDelete
+                                            onClick={() => excluir(item.id)}
+                                            type='submit'
+                                            className='text-danger' />
+                                    </td>
+                                    <td>{item.nome}</td>
+                                    <td>{item.cpf}</td>
+                                    <td>{item.matricula}</td>                                    
+                                    <td>{item.email}</td>
+                                    <td>{item.telefone}</td>
+                                    <td>{item.cep}</td>
+                                    <td>{item.logradouro}</td>
+                                    <td>{item.complemento}</td>
+                                    <td>{item.numero}</td>
+                                    <td>{item.bairro}</td>
+
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+
+                </Col>
+
+            </Row>
+        </Pagina>
+    )
+}
+
+export default index
